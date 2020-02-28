5cf84e45d810a10d11b9dd4b9bda270c6f8b298b
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-02-27 22:32:17 -0500

    Added test program for quadcopter

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "src/CartPoleDynamics.h"
+#include "src/QuadcopterDynamics.h"
 #include "src/DDPController.h"
 #include "src/QuadraticCost.h"
 #include "src/Simulation.h"
@@ -8,25 +8,33 @@ const double pi = atan(1.0) * 4.0;
 
 int main() {
 
-    CartPoleDynamics* cart_pole_dynamics_ptr = new CartPoleDynamics(1.0, 0.01, 0.25);
 
-    arma::mat Q_f = 10.0 * arma::eye<arma::mat>(4, 4);
-    arma::mat R = 0.1 * arma::eye<arma::mat>(1, 1);
-    QuadraticCost* quadratic_cost_ptr = new QuadraticCost(Q_f, R);
+    double m = 0.468;
+    double g = 9.81;
+    double l = 0.225;
+    double k = 0.00000298;
+    double b = 0.000000114;
+    double I_xx = 0.004856;
+    double I_yy = 0.004856;
+    double I_zz = 0.008801;
 
-    DDPController* ddp_controller_ptr = new DDPController(cart_pole_dynamics_ptr,quadratic_cost_ptr, 200, 100, 0.1);
+    QuadcopterDynamics* quadcopter_dynamics_ptr = new QuadcopterDynamics(m, g, l, k, b, I_xx, I_yy, I_zz);
 
-    arma::vec x_0;
-    x_0 << 0.0 << arma::endr << 0.0 << arma::endr << 0.0 << arma::endr << 0.0 << arma::endr;
+    arma::vec x = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
-    arma::vec x_star;
-    x_star << 0.0 << arma::endr << 0.0 << arma::endr << pi << arma::endr << 0.0 << arma::endr;
+    double dt = 0.01;
 
-    System* system_ptr = new System(cart_pole_dynamics_ptr, ddp_controller_ptr);
+    double u_hover = 1.01*(m*g/4)/k;
+    arma::vec u = {1.001*u_hover, u_hover, u_hover, u_hover};
 
-    Simulation* simulation_ptr = new Simulation(system_ptr, 0.05, x_0, x_star, 0.5);
-
-    simulation_ptr->simulate(3.0);
+    for (int k = 0; k < 1000; k++) {
+        x = x + quadcopter_dynamics_ptr->F(x, u, k*dt) * dt;
+        std::cout << k*dt << "," << u[0] << "," << u[1] << "," << u[2] << "," << u[3];
+        for (int i = 0; i < 12; i++) {
+            std::cout << "," << x[i];
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
