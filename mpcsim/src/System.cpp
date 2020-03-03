d6c593b61d12f116bb01f4ea2d46eaa4ccb33c7d
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-03-02 23:26:52 -0500

    Added debug code to System

--- mpcsim/src/System.cpp
+++ mpcsim/src/System.cpp
@@ -12,4 +12,12 @@ void System::operator()(const arma::vec &x, arma::vec &dxdt, const double t) {
 void System::updateControl(arma::vec x, arma::vec x_star, double t, double horizon) {
     // Use the controller to compute the best control input for the current state and assign it to the member variable
     this->m_u = this->m_controller_ptr->computeOptimalControl(x, t, x_star, t + horizon);
+
+#ifdef DEBUG
+    for (long long unsigned int i = 0; i < this->m_u.n_elem; i++) {
+        if (std::isnan(this->m_u(i))) {
+            std::cout << "error: computed optimal control has NaN\n"; exit(-1);
+        }
+    }
+#endif
 }
