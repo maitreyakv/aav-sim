370224cab25cf0ab07960881e454554d197e09fa
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-03-11 14:38:19 -0400

    Removed unused variables in Quadratic Cost

--- mpcsim/src/QuadraticCost.cpp
+++ mpcsim/src/QuadraticCost.cpp
@@ -14,42 +14,42 @@ arma::vec QuadraticCost::phi_x(arma::vec x_f, arma::vec x_star) {
     return this->m_Q_f * (x_f - x_star);
 }
 
-arma::mat QuadraticCost::phi_xx(arma::vec x_f, arma::vec x_star) {
+arma::mat QuadraticCost::phi_xx(arma::vec, arma::vec) {
     // Compute and return the hessian of the quadratic cost function
     return this->m_Q_f;
 }
 
-double QuadraticCost::L(arma::vec x, arma::vec u, double dt) {
+double QuadraticCost::L(arma::vec, arma::vec u, double dt) {
     // Compute and return the transition cost
     return arma::as_scalar( 0.5 * u.t() * this->m_R * u * dt );
 }
 
-arma::vec QuadraticCost::L_x(arma::vec x, arma::vec u, double dt) {
+arma::vec QuadraticCost::L_x(arma::vec x, arma::vec, double) {
     // Compute and return the gradient of the transition cost with respect to the state
     return arma::zeros<arma::vec>(x.n_elem);
 }
 
-arma::vec QuadraticCost::L_u(arma::vec x, arma::vec u, double dt) {
+arma::vec QuadraticCost::L_u(arma::vec, arma::vec u, double dt) {
     // Compute and return the gradient of the transition cost with respect to the control
     return this->m_R * u * dt;
 }
 
-arma::mat QuadraticCost::L_xx(arma::vec x, arma::vec u, double dt) {
+arma::mat QuadraticCost::L_xx(arma::vec x, arma::vec, double) {
     // Compute and return the Hessian of the transition cost with respect to the state
     return arma::zeros<arma::mat>(x.n_elem, x.n_elem);
 }
 
-arma::mat QuadraticCost::L_uu(arma::vec x, arma::vec u, double dt) {
+arma::mat QuadraticCost::L_uu(arma::vec, arma::vec, double dt) {
     // Compute and return the Hessian of the transition cost with respect to the control
     return this->m_R * dt;
 }
 
-arma::mat QuadraticCost::L_xu(arma::vec x, arma::vec u, double dt) {
+arma::mat QuadraticCost::L_xu(arma::vec x, arma::vec u, double) {
     // Compute and return the double double gradient of the transition cost with respect to the state and then control
     return arma::zeros<arma::mat>(x.n_elem, u.n_elem);
 }
 
-arma::mat QuadraticCost::L_ux(arma::vec x, arma::vec u, double dt) {
+arma::mat QuadraticCost::L_ux(arma::vec x, arma::vec u, double) {
     // Compute and return the double double gradient of the transition cost with respect to the control and then state
     return arma::zeros<arma::mat>(u.n_elem, x.n_elem);
 }
